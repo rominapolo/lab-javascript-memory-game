c9ff2d80b745a399bdf1d23fc6a557194c3ae04d
Romina Polo <rominalpolo@gmail.com>
2022-08-04 21:58:48 -0400

    memory game update

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,40 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
+
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards) {
+      return undefined
+    }
+    for (let i = this.cards.length - 1; i > 0; i--)  {
+    let j = Math.floor(Math.random() * i);
+    let temp = this.cards[i];
+    this.cards[i] = this.cards[j];
+    this.cards[j] = temp;
+  }
+  return this.cards;
   }
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
+checkIfPair(card1, card2) {
+    if (card1 === card2){
+      this.pairsGuessed++;
+      this.pairsClicked++
+      return true
+    } else {
+      this.pairsClicked++;
+      return false
+    }
   }
-
   checkIfFinished() {
-    // ... write your code here
+    if(this.cards.length === 24){
+    return true
+    } else {
+    return false
+    }
   }
 }
